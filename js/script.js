8c3a2a16432f1fe3372732bb507ae4c6e61578db
Ali Bahadoran <alibahadoran.eng@gmail.com>
2022-09-21 14:16:29 +0430

    Start Project

---
+++ js/script.js
@@ -0,0 +1,12 @@
+﻿const contextMenu = document.getElementById("contextMenu");
+
+function contextHandler(event) {
+    event.preventDefault();
+
+    if (contextMenu.style.display === "none") {
+        contextMenu.style.left = event.pageX + "px";
+        contextMenu.style.top = event.pageY + "px";
+        contextMenu.style.display = "block";
+    }
+}
+document.body.addEventListener("contextmenu", contextHandler);
